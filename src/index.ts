45619da2d360aaf0d5ca06d6d0fb6827f89c8dc1
Tanara Morgana <tanaramorgana@gmail.com>
2023-09-17 21:14:22 -0300

    corrigindo bug na validacao do salario

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,49 @@
-import { Grades, Wallet, Product, User, Director, employee } from "./types/types"
-
+import {
+  Grades,
+  Wallet,
+  Product,
+  User,
+  Director,
+  Employee,
+} from "./types/types";
 
 // 1. Crie uma função que receba 2 números e retorne um objeto
 // contendo a média e também um indicador booleano de
 // aprovado/reprovado. Considere aprovado com média >= 6.
 
-
 function isApproved(n1: number, n2: number) {
   const average = (n1 + n2) / 2;
   const approved = average >= 6;
-  
-    return {
-      'media': average,
-      'apovado': approved
-    }
+
+  return {
+    media: average,
+    apovado: approved,
+  };
 }
 
 console.log(isApproved(8, 9));
-
 
 // // 2. Crie uma função que receba uma lista de objetos contendo nota e
 // // peso, realize a média das notas considerando o peso. Exemplos:
 // // Lista com 2 notas: (N1*P1) + (N2*P2) / 2 = Resultado
 // // Lista com 3 notas: (N1*P1) + (N2*P2) + (N3*P3) / 3 = Resultado
 
-const arrayOfGrades: Grades[] = [{
-      nota: 8,
-      peso: 1
+const arrayOfGrades: Grades[] = [
+  {
+    nota: 8,
+    peso: 1,
   },
   {
-      nota: 7,
-      peso: 2
+    nota: 7,
+    peso: 2,
   },
   {
-      nota: 6,
-      peso: 3
-  }];
+    nota: 6,
+    peso: 3,
+  },
+];
 
-function gradesAverage(arrayOfGrades: Grades[]): number{
+function gradesAverage(arrayOfGrades: Grades[]): number {
   let addGrades = 0;
   for (const grade of arrayOfGrades) {
     addGrades += grade.nota * grade.peso;
@@ -47,7 +53,6 @@ function gradesAverage(arrayOfGrades: Grades[]): number{
 
 console.log(gradesAverage(arrayOfGrades));
 
-
 // // 3. Crie um programa que simule uma carteira de dinheiro. Este
 // // programa vai precisar ter uma carteira contendo saldo, transações
 // // de entrada e saídas. Ou seja, será um objeto com estas
@@ -56,60 +61,56 @@ console.log(gradesAverage(arrayOfGrades));
 // // um erro ou avisar.
 
 const wallet: Wallet = {
-    balance: 100,
-    transactions: []
-}
+  balance: 100,
+  transactions: [],
+};
 
 function transaction(description: string, value: number): string | number {
-    if (description === 'entrada') {
-      wallet.transactions.push({ description, value });
-      return wallet.balance += value;
+  if (description === "entrada") {
+    wallet.transactions.push({ description, value });
+    return (wallet.balance += value);
+  }
+
+  if (description === "saida") {
+    if (wallet.balance <= 0) {
+      return "saldo insuficiente";
     }
-  
-    if (description === 'saida') {
-      if (wallet.balance <= 0) {
-        return 'saldo insuficiente';
-      }
-      wallet.transactions.push({ description, value });
-      return wallet.balance -= value;
-    }
-  
-    return 'transação inválida';
+    wallet.transactions.push({ description, value });
+    return (wallet.balance -= value);
+  }
+
+  return "transação inválida";
 }
 
-wallet.transactions.push()
-console.log(transaction('entrada', 200));
-
-
-
+wallet.transactions.push();
+console.log(transaction("entrada", 200));
 
 // 4. Crie um programa para cadastrar, listar e excluir produtos de uma
 // lista com tipagem de Produto.
 
 const products: Product[] = [];
 
-function listProducts (products: Product[]): void{
+function listProducts(products: Product[]): void {
   products.map((product) => {
     console.log(product);
-    
-  })
+  });
 }
 
-function registerProduct (product: Product): void{
+function registerProduct(product: Product): void {
   products.push(product);
 }
 
-registerProduct({name: 'banana'});
-registerProduct({name: 'maça'});
+registerProduct({ name: "banana" });
+registerProduct({ name: "maça" });
 listProducts(products);
 
-function excludeProduct (product: string): void{
+function excludeProduct(product: string): void {
   let findProduct = products.findIndex((value) => value.name == product);
 
   products.splice(findProduct, 1);
 }
 
-excludeProduct('banana');
+excludeProduct("banana");
 
 // // 5. Crie um programa para mostrar informações de usuários (User) de
 // // uma empresa. Crie o tipo User com as seguintes propriedades:
@@ -118,27 +119,28 @@ excludeProduct('banana');
 // // a. “Daphne, 23 anos, analista de TI, salário R$ 1000”
 // // b. “Daphne, 23 anos, analista de TI, salário N/A”
 
-const users: User[] =  [{
-  name: 'Maria',
-  age: 18,
-  occupation: 'analista de TI'
-},
-{
-  name: 'Daphne',
-  age: 23,
-  occupation: 'analista de TI',
-  salary: 2000
-}];
+const users: User[] = [
+  {
+    name: "Maria",
+    age: 18,
+    occupation: "analista de TI",
+  },
+  {
+    name: "Daphne",
+    age: 23,
+    occupation: "analista de TI",
+    salary: 2000,
+  },
+];
 
-function showUser(users: User[]){
-
- for (const user of users) {
-    if(!user.salary){
-      user.salary = 'N/A'
-    }
-    console.log(`${user.name}, ${user.age} anos, ${user.occupation}, salário ${user.salary}`);
-    
-}
+function showUser(users: User[]) {
+  for (const user of users) {
+    console.log(
+      `${user.name}, ${user.age} anos, ${user.occupation}, salário ${
+        user.salary === undefined ? "N/A" : user.salary
+      }`
+    );
+  }
 }
 
 showUser(users);
@@ -151,24 +153,27 @@ showUser(users);
 // a. “Diretor(a) Daphne, 23 anos, comissão nível 5, salário R$ 1000”
 // b. “Diretor(a) Daphne, 23 anos, comissão nível 5, salário N/A”
 
-const directors: Director[] =  [{
-  name: 'Paulo',
-  age: 30,
-  commission: 5,
-  salary: 5000
-},
-{
-  name: 'José',
-  age: 50,
-  commission: 4
-}]
+const directors: Director[] = [
+  {
+    name: "Paulo",
+    age: 30,
+    commission: 5,
+    salary: 5000,
+  },
+  {
+    name: "José",
+    age: 50,
+    commission: 4,
+  },
+];
 
-function showDirector(directors: Director[]){
+function showDirector(directors: Director[]) {
   for (const director of directors) {
-     if(!director.salary){
-      director.salary = 'N/A'
-    }
-    console.log(`Diretor(a) ${director.name}, ${director.age} anos, comissão nível ${director.commission}, salário ${director.salary}`);
+    console.log(
+      `Diretor(a) ${director.name}, ${director.age} anos, comissão nível ${
+        director.commission
+      }, salário ${director.salary === undefined ? "N/A" : director.salary}`
+    );
   }
 }
 
@@ -180,38 +185,45 @@ showDirector(directors);
 // a. O mesmo que o exercício 5, em caso de objeto User.
 // b. O mesmo que o exercício 6, em caso de objeto Diretor.
 
-const employees = [{
-  name: 'Paulo',
-  age: 30,
-  commission: 5,
-  salary: 5000
-},
-{
-  name: 'Mario',
-  age: 40,
-  commission: 3
-},
-{
-  name: 'Daphne',
-  age: 23,
-  occupation: 'analista de TI',
-  salary: 2000
-},
-{
-  name: 'Joana',
-  age: 30,
-  occupation: 'analista de TI'
-}];
+const employees: Employee[] = [
+  {
+    name: "Paulo",
+    age: 30,
+    commission: 5,
+    salary: 5000,
+  },
+  {
+    name: "Mario",
+    age: 40,
+    commission: 3,
+  },
+  {
+    name: "Daphne",
+    age: 23,
+    occupation: "analista de TI",
+    salary: 2000,
+  },
+  {
+    name: "Joana",
+    age: 30,
+    occupation: "analista de TI",
+  },
+];
 
-function showEmployee(employees: employee[]){
-  for(const employee of employees){
-    if(!employee.salary){
-      employee.salary = 'N/A'
-    }
-    if('commission' in employee){
-      console.log(`Diretor(a) ${employee.name}, ${employee.age} anos, comissão nível ${employee.commission}, salário ${employee.salary}`);
+function showEmployee(employees: Employee[]) {
+  for (const employee of employees) {
+    if ("commission" in employee) {
+      console.log(
+        `Diretor(a) ${employee.name}, ${employee.age} anos, comissão nível ${
+          employee.commission
+        }, salário ${employee.salary === undefined ? "N/A" : employee.salary}`
+      );
     } else {
-      console.log(`${employee.name}, ${employee.age} anos, ${employee.occupation}, salário ${employee.salary}`);
+      console.log(
+        `${employee.name}, ${employee.age} anos, ${
+          employee.occupation
+        }, salário ${employee.salary === undefined ? "N/A" : employee.salary}`
+      );
     }
   }
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,29 +1,29 @@
 export interface Grades {
-    nota: number,
-    peso: number
-  }
+  nota: number;
+  peso: number;
+}
 
-export interface Wallet{
-    balance: number,
-    transactions: {description: string, value: number}[];
+export interface Wallet {
+  balance: number;
+  transactions: { description: string; value: number }[];
 }
 
 export interface Product {
-    name: string
+  name: string;
 }
 
-export interface User{
-    name: string,
-    age: number,
-    occupation: string,
-    salary?: number | string
-};
+export interface User {
+  name: string;
+  age: number;
+  occupation: string;
+  salary?: number | string;
+}
 
-export interface Director{
-    name: string,
-    age: number,
-    commission: number,
-    salary?: number | string
-};
+export interface Director {
+  name: string;
+  age: number;
+  commission: number;
+  salary?: number | string;
+}
 
-export type employee = User | Director;
+export type Employee = User | Director;
